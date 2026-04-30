a32910b5335910137bc23f334da1c52910e4dc60
Kamblossi <newton.ochieng97@gmail.com>
2026-04-30 15:28:28 +0000

    Add PrismERP brand constants

---
+++ prism_brand/branding/constants.py
@@ -0,0 +1,28 @@
+# PrismERP Master Brand Constants
+
+BRAND_NAME = "PrismERP"
+BRAND_COMPANY = "PrismTechCo"
+BRAND_PUBLISHER = "PrismTechCo"
+BRAND_TAGLINE = "Business operations, simplified."
+BRAND_DESCRIPTION = "PrismERP is a hosted ERP platform for growing businesses."
+BRAND_VERSION = "1.0.0" 
+
+# Visual Identity
+BRAND_LOGO = "/assets/prism_brand/images/prismerp-logo.svg"
+BRAND_FAVICON = "/assets/prism_brand/images/favicon.svg"
+BRAND_PRIMARY_COLOR = "#1f3b73"
+BRAND_PRIMARY_DARK = "#14264a"
+
+# Consolidated Email Strategy
+BRAND_MAIN_EMAIL = "erp@prismtechco.com"
+BRAND_SUPPORT_EMAIL = "erp-support@prismtechco.com"
+BRAND_BILLING_EMAIL = "erp-billing@prismtechco.com"
+BRAND_FROM_EMAIL = "PrismERP <erp@prismtechco.com>"
+
+# Navigation & Web Presence
+BRAND_WEBSITE = "https://prismtechco.com"
+BRAND_PRIVACY_URL = "https://prismtechco.com/privacy"
+BRAND_TERMS_URL = "https://prismtechco.com/terms"
+BRAND_LOGIN_ROUTE = "/login"
+BRAND_APP_HOME = "/app"
+BRAND_DASHBOARD_HOME = "/app/workspace"
