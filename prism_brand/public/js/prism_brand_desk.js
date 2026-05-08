49cab53e6b029e911c06afcfa6a83f13800a550d
Kamblossi <newton.ochieng97@gmail.com>
2026-05-08 02:28:00 +0000

    Fix About dialog override: target frappe.ui.misc.about function

--- prism_brand/public/js/prism_brand_desk.js
+++ prism_brand/public/js/prism_brand_desk.js
@@ -12,7 +12,7 @@ What it does:
 - Sets document title once
 - Adds prismerp-desk body class once
 - Replaces workspace sidebar subtitle "ERPNext" with "PrismERP" (targeted, on route change)
-- Overrides the About dialog with custom PrismERP content (targeted, event-driven)
+- Overrides the About dialog with custom PrismERP content (replaces frappe.ui.misc.about)
 */
 
 (function () {
@@ -62,38 +62,42 @@ What it does:
 
   /* --- About dialog override --- */
   function overrideAboutDialog() {
-    if (typeof frappe === "undefined" || !frappe.ui || !frappe.ui.toolbar) return;
-
-    var AboutDialog = frappe.ui.toolbar.AboutDialog;
-    if (!AboutDialog) return;
+    if (typeof frappe === "undefined") return;
+    frappe.provide("frappe.ui.misc");
 
     var aboutHtml = [
-      "<div class='prismerp-about' style='padding:16px;line-height:1.8'>",
-      "<h3 style='margin-bottom:8px'>PrismERP</h3>",
-      "<p style='margin-bottom:16px;font-size:14px;color:#666'>Enterprise Resource Planner to Help you Grow your Business</p>",
-      "<p><strong>Website:</strong> <a href='https://prismtechco.com' target='_blank' rel='noopener noreferrer'>https://prismtechco.com</a></p>",
-      "<p><strong>LinkedIn:</strong> <a href='https://www.linkedin.com/in/newton-ochieng-18703a12b' target='_blank' rel='noopener noreferrer'>www.linkedin.com/in/newton-ochieng-18703a12b</a></p>",
-      "<p><strong>X:</strong> <a href='https://x.com/PrismTechCo' target='_blank' rel='noopener noreferrer'>https://x.com/PrismTechCo</a></p>",
-      "<p><strong>Email:</strong> <a href='mailto:erp@prismtechco.com'>erp@prismtechco.com</a></p>",
-      "<hr>",
-      "<p><strong>Apps Version</strong></p>",
-      "<p>PrismERP: v0.1.0</p>",
-      "<hr>",
-      "<p>\u00a9 PrismTechCo</p>",
+      "<div>",
+      "  <p><strong>PrismERP</strong></p>",
+      "  <p style='margin-bottom:16px;font-size:14px;color:#666'>Enterprise Resource Planner to Help you Grow your Business</p>",
+      "",
+      "  <p><i class='fa fa-globe fa-fw'></i> <strong>Website:</strong> <a href='https://prismtechco.com' target='_blank'>https://prismtechco.com</a></p>",
+      "  <p><i class='fa fa-linkedin fa-fw'></i> <strong>LinkedIn:</strong> <a href='https://www.linkedin.com/in/newton-ochieng-18703a12b' target='_blank'>www.linkedin.com/in/newton-ochieng-18703a12b</a></p>",
+      "  <p><svg xmlns='http://www.w3.org/2000/svg' width='18' height='14' fill='currentColor' class='bi bi-twitter-x' viewBox='0 0 18 16'><path d='M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z'/></svg> <strong>X:</strong> <a href='https://x.com/PrismTechCo' target='_blank'>https://x.com/PrismTechCo</a></p>",
+      "  <p><i class='fa fa-envelope fa-fw'></i> <strong>Email:</strong> <a href='mailto:erp@prismtechco.com'>erp@prismtechco.com</a></p>",
+      "",
+      "  <hr>",
+      "",
+      "  <div class='d-flex align-items-center justify-content-between'>",
+      "    <h4>Apps Version</h4>",
+      "  </div>",
+      "  <div id='about-app-versions'>PrismERP: v0.1.0</div>",
+      "",
+      "  <hr>",
+      "",
+      "  <p class='text-muted'>&copy; PrismTechCo</p>",
       "</div>"
     ].join("");
 
-    AboutDialog.prototype.show = function () {
-      if (!this.dialog) {
-        this.dialog = new frappe.ui.Dialog({
-          title: __("About"),
-          fields: [
-            { fieldtype: "HTML", fieldname: "about_html" }
-          ]
-        });
+    frappe.ui.misc.about = function () {
+      if (frappe.ui.misc.about_dialog) {
+        frappe.ui.misc.about_dialog.show();
+        return;
       }
-      this.dialog.get_field("about_html").$wrapper.html(aboutHtml);
-      this.dialog.show();
+
+      var dialog = new frappe.ui.Dialog({ title: __("PrismERP") });
+      $(dialog.body).html(aboutHtml);
+      frappe.ui.misc.about_dialog = dialog;
+      dialog.show();
     };
   }
 
